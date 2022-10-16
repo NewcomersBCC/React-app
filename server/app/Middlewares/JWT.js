3d1e7774d45e61a94dc0a6c6ac33ea460f6d0887
tavaresl <tavaresl@uni.coventry.ac.uk>
2022-10-16 22:07:38 +0100

    validating user token

--- server/app/Middlewares/JWT.js
+++ server/app/Middlewares/JWT.js
@@ -5,20 +5,20 @@ export default {
   async createToken(user) {
     const accessToken = await sign(
       { email: user.email, id: user.id },
-      "jwtsecrets"
+      process.env.JWT_SECRET
     );
     return accessToken;
   },
 
   async validateToken(req, res, next) {
-    const accessToken = req.cookies("access-token");
+    const accessToken = req.cookies["access-token"];
 
     if (!accessToken) {
-      return res.status(400).send("User not authenticated!");
+      return res.status(401).send("User not authenticated!");
     }
 
     try {
-      const validToken = await verify(accessToken, "jwtsecrets");
+      const validToken = await verify(accessToken, process.env.JWT_SECRET);
       if (validToken) {
         req.authenticated = true;
         return next();
